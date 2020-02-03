16ef405bbdbef14d30ac28e2ef4b316b1c1554eb
mcardiel <maribel1cardiel@gmail.com>
2020-02-03 15:04:59 -0800

    added button to rankingActivity

--- App/app/src/main/java/com/example/scavengerhuntapp/RankingsActivity.java
+++ App/app/src/main/java/com/example/scavengerhuntapp/RankingsActivity.java
@@ -4,11 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+
+
+
+
 public class RankingsActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rankings);
+        addListenerOnButton();
     }
+
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button1);
+
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, MainActivity.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+    }
+
+
 }
