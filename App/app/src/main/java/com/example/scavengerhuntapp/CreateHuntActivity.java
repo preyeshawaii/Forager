d968dc893e57732bbbb31988a7e43066e49e34c9
Paulina <preyeshawaii@gmail.com>
2020-02-06 16:52:13 -0800

    Added button to CreateHuntActivity java

--- App/app/src/main/java/com/example/scavengerhuntapp/CreateHuntActivity.java
+++ App/app/src/main/java/com/example/scavengerhuntapp/CreateHuntActivity.java
@@ -2,7 +2,11 @@ package com.example.scavengerhuntapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateHuntActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class CreateHuntActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_hunt);
+        addListenerOnButton();
     }
+
+    public void addListenerOnButton() {
+        final Context context = this;
+        Button premadeChallButton = (Button) findViewById(R.id.premade_chall_button);
+        premadeChallButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, PremadeHuntsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
