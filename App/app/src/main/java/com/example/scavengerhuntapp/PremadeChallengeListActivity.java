31a94ef049ad37e913cf769f15a32b028fd79ff7
Paulina <preyeshawaii@gmail.com>
2020-02-08 20:16:52 -0800

    Added custom premade challenges list items and hardcoded samples

--- App/app/src/main/java/com/example/scavengerhuntapp/PremadeChallengeListActivity.java
+++ App/app/src/main/java/com/example/scavengerhuntapp/PremadeChallengeListActivity.java
@@ -2,13 +2,112 @@ package com.example.scavengerhuntapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class PremadeChallengeListActivity extends AppCompatActivity {
+    SharedPreferences sharedpreferences;
+    int[] ICONS = {R.drawable.icecream, R.drawable.group, R.drawable.dog};
+    String[] CHALLENGES = {"Eat the Earthquake", "Loudly advertise See's Chocolate", "Walk someone's dog"};
+    String[] LOCATIONS = {"Ghirardelli Square", "Chinatown", "Golden Gate Park"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_premade_challenge_list);
+
+        final ListView premadeChallengesList=(ListView)findViewById(R.id.premade_challenge_list);
+        Button addToHuntButton = (Button)findViewById(R.id.add_to_hunt_button);
+
+        final CustomAdapter customAdapter=new CustomAdapter();
+        premadeChallengesList.setChoiceMode(premadeChallengesList.CHOICE_MODE_MULTIPLE);
+        premadeChallengesList.setAdapter(customAdapter);
+
+        addToHuntButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // TODO(@Jorge): Store the selected challenges in database for access elsewhere
+                // Commented code shows how to store as strings, but we need to store as objects
+//                String selected = "";
+                int cntChoice = premadeChallengesList.getCount();
+
+                SparseBooleanArray sparseBooleanArray = premadeChallengesList.getCheckedItemPositions();
+                for(int i = 0; i < sparseBooleanArray.size(); i++){
+                    System.out.println(sparseBooleanArray.valueAt(i));
+                    if (sparseBooleanArray.valueAt(i)) {
+                        System.out.println("Checking list while adding:" +
+                                premadeChallengesList.getItemAtPosition(i).toString());
+
+                    }
+//                    if(sparseBooleanArray.get(i)) {
+//                        selected += premadeChallengesList.getItemAtPosition(i);
+//                        System.out.println("Checking list while adding:" + myList.getItemAtPosition(i).toString());
+//                        SaveSelections();
+//                    }
+
+                }
+                Intent intent = new Intent(PremadeChallengeListActivity.this, PremadeHuntsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+//    private void SaveSelections() {
+//        // save the selections in the shared preference in private mode for the user
+//        SharedPreferences.Editor prefEditor = sharedpreferences.edit();
+//        String savedItems = getSavedItems();
+//        prefEditor.putString(MyPREFERENCES.toString(), savedItems);
+//        prefEditor.commit();
+//    }
+
+    class CustomAdapter extends BaseAdapter{
+        private CheckBox checkBox;
+
+        @Override
+        public int getCount() {
+            return ICONS.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return i;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        public Boolean isChecked(int i){
+           return checkBox.isChecked();
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view = getLayoutInflater().inflate(R.layout.challenge_custom_view,null);
+            // Populate list view
+            ImageView imageView = (ImageView)view.findViewById(R.id.iconImageView);
+            TextView challengeTextView = (TextView)view.findViewById(R.id.challengeTextView);
+            TextView challengeLocationTextView = (TextView)view.findViewById(R.id.challengeLocationTextView);
+            checkBox = findViewById(R.id.checkBox);
+
+            imageView.setImageResource(ICONS[i]);
+            challengeTextView.setText(CHALLENGES[i]);
+            challengeLocationTextView.setText(LOCATIONS[i]);
+
+            return view;
+        }
     }
 }
--- App/app/src/main/java/com/example/scavengerhuntapp/PremadeHuntsActivity.java
+++ App/app/src/main/java/com/example/scavengerhuntapp/PremadeHuntsActivity.java
@@ -2,13 +2,44 @@ package com.example.scavengerhuntapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class PremadeHuntsActivity extends AppCompatActivity {
+
+    String[] locationsArray = {"San Francisco"};
+    ListView locationsListView;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_premade_hunts);
+
+        adapter = new ArrayAdapter<>(this,
+                R.layout.single_list_item, R.id.premade_hunt_location, locationsArray);
+
+        locationsListView = (ListView)findViewById(R.id.locations_list);
+        locationsListView.setAdapter(adapter);
+        locationsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(PremadeHuntsActivity.this, PremadeChallengeListActivity.class);
+                // Need to put the location as an extra so we can use that to get the premade
+                // challenges that correspond to that location from the database
+//                intent.putExtra(Hunt.KEY_HUNT_ID, huntID);
+//                intent.putExtra(Hunt.KEY_HUNT_NAME, huntName);
+//                Log.w(TAG, huntID + ": " + huntName);
+                startActivity(intent);            }
+        });
+
     }
+
+
 }
