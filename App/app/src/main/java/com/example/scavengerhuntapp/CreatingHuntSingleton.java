a6396655e43af80b2d6c56bec06444ccbf4cf3aa
mcardiel <maribel1cardiel@gmail.com>
2020-03-04 15:53:36 -0800

    added all the images

--- App/app/src/main/java/com/example/scavengerhuntapp/CreatingHuntSingleton.java
+++ App/app/src/main/java/com/example/scavengerhuntapp/CreatingHuntSingleton.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class CreatingHuntSingleton {
     private static CreatingHuntSingleton creatingHuntSingleton = null;
 
-    final static int[] ICONS = {R.drawable.icecream, R.drawable.group, R.drawable.dog};
+    final static int[] ICONS = {R.drawable.icecream, R.drawable.group, R.drawable.dog, R.drawable.a1};
     final static String[] CHALLENGES = {"Eat the Earthquake", "Loudly advertise See's Chocolate", "Walk someone's dog"};
     final static String[] LOCATIONS = {"Ghirardelli Square", "Chinatown", "Golden Gate Park"};
 
